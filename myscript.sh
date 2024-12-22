a7cc5fac144e473eb5b9ff52bdd1f84fefe0581d
david.peter <david.peter@pronovix.com>
2024-12-22 17:37:24 +0100

    Add practice script to the repo.

---
+++ myscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is a practice script
+echo "Welcome to Bash Scripting!"
+name="Tech writer"
+favorite_color="blue"
+echo "Hello, I'm a $name! My favorite color is $favorite_color."
